ba40bbf5655032c1fdeb423c567469e4c45691eb
Junyoung <elecegg@gmail.com>
2014-04-17 11:29:33 -0400

    added normalization and divided the dataset into Training/Testing sets

--- pylearn2/datasets/timit.py
+++ pylearn2/datasets/timit.py
@@ -3,7 +3,7 @@ Utitlies for accessing TIMIT dataset.
 """
 __authors__ = ["Dustin Webb"]
 __copyright__ = "Copyright 2014, Universite de Montreal"
-__credits__ = ["Dustin Webb"]
+__credits__ = ["Dustin Webb", "Junyoung Chung"]
 __license__ = "3-clause BSD"
 __maintainer__ = "Dustin Webb"
 __email__ = "webbd@iro"
@@ -13,7 +13,8 @@ import sys
 import numpy
 from pylearn2.datasets import DenseDesignMatrix
 from pylearn2.utils import serial
-import pdb
+from pylearn2.space import CompositeSpace, Conv2DSpace, VectorSpace
+import ipdb
 
 
 class TIMITPerPhone(DenseDesignMatrix):
@@ -24,11 +25,18 @@ class TIMITPerPhone(DenseDesignMatrix):
     _default_seed = 1
     _data_dir = '/data/lisa/data/timit/readable/per_phone'
 
-    def __init__(
-        self, phone, frame_length, target_width=1,
-        max_examples=None, random_examples=False,
-        rng=None
-    ):
+    def __init__(self,
+                 phone,
+                 frame_length,
+                 target_width=1,
+                 max_examples=None,
+                 random_examples=False,
+                 test=False,
+                 unit_norm=False,
+                 normalize=False,
+                 mean=None,
+                 std=None,
+                 rng=None):
         """
         Parameters
         ----------
@@ -48,6 +56,14 @@ class TIMITPerPhone(DenseDesignMatrix):
         assert(file in files)
         self.phone = phone
         self.file = file
+
+        # Flags for tr/te set and normalization
+        self.test = test
+        self.unit_norm = unit_norm
+        self.normalize = normalize
+        self._mean = mean
+        self._std = std
+        self._mean_norm = 0
 
         assert(frame_length > 0)
         self.frame_length = frame_length
@@ -75,6 +91,12 @@ class TIMITPerPhone(DenseDesignMatrix):
 
     def _load_data(self):
         data = serial.load(os.path.join(self._data_dir, self.file))
+
+        if self.test is True:
+            data = data[-300:]
+        else:
+            data = data[:-300]
+
         idxs = numpy.arange(len(data))
 
         if self.random_examples:
@@ -82,6 +104,34 @@ class TIMITPerPhone(DenseDesignMatrix):
 
         if self.max_examples is not None:
             idxs = idxs[:self.max_examples]
+
+        if self.unit_norm is True:
+            for i, example in enumerate(data):
+                exp_euclidean_norm = numpy.sqrt(numpy.square(example).sum())
+                data[i] = example / exp_euclidean_norm
+                self._mean_norm += exp_euclidean_norm
+            self._mean_norm /= data.shape[0]
+
+        if self.normalize is True:
+            if self._mean is not None and self._std is not None:
+                for i, example in enumerate(data):
+                    data[i] = (example - self._mean) / self._std
+            else:
+                exp_sum = 0
+                exp_cnt = 0
+                for i, example in enumerate(data):
+                    exp_sum += example.sum()
+                    exp_cnt += len(example)
+                exp_mean = exp_sum / exp_cnt
+                exp_sqr_sum = 0
+                for i, example in enumerate(data):
+                    exp_sqr_sum += numpy.cast[float](numpy.square(example -
+                                                     exp_mean).sum())
+                exp_std = numpy.sqrt(exp_sqr_sum / exp_cnt)
+                self._mean = exp_mean
+                self._std = exp_std
+                for i, example in enumerate(data):
+                    data[i] = (example - self._mean) / self._std
 
         # Do math to determine how many samples there will be and make space
         total_rows = 0
@@ -91,8 +141,6 @@ class TIMITPerPhone(DenseDesignMatrix):
 
         X = numpy.zeros((total_rows, self.frame_length))
         y = numpy.zeros((total_rows, self.target_width))
-
-        pdb.set_trace()
 
         count = 0
         for i in idxs:
@@ -112,9 +160,20 @@ def testload_data():
     """
     Routine for testing the loading of TIMIT phones
     """
-    t = TIMITPerPhone('aa', 10, max_examples=2, random_examples=True)
-
-    pdb.set_trace()
+    t = TIMITPerPhone(phone='aa',
+                      frame_length=240,
+                      max_examples=100,
+                      random_examples=False,
+                      unit_norm=True,
+                      normalize=True)
+    return t
 
 if __name__ == "__main__":
-    testload_data()
+    data_specs = (CompositeSpace([VectorSpace(dim=240),
+                                  VectorSpace(dim=1)]),
+                  ('features', 'targets'))
+    t = testload_data()
+    it = t.iterator(mode='sequential', data_specs=data_specs,
+                    num_batches=None, batch_size=100)
+    X, y = it.next()
+    ipdb.set_trace()
